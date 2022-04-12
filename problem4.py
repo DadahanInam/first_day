5a1258b698bb8e3c16e9a2498d4e31b7062e134c
DadahanInam <103410287+DadahanInam@users.noreply.github.com>
2022-04-12 23:02:01 +0600

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,8 @@
+menu = {'lagman' : 120, 'plov' : 120, 'borsh' : 100,}
+menu.update({'besh_barmak' : 130,})
+menu.update({'drinks' : ['Coca-Cola','Fanta','Sprite']})
+menu['lagman'] = '135'
+menu.pop('borsh')
+print(menu)
+
+
